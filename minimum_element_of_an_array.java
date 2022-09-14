ff0629dfbf005ec802ae289409b6c463a279119e
K_SaiSriram <84832741+Sriram5-coder@users.noreply.github.com>
2022-09-14 06:17:21 +0530

    import java.util.*; class Sample {     public static void main(String args[])     {         int N;         Scanner sc=new Scanner(System.in);         N=sc.nextInt();         int arr[]=new int[N];         for(int i=0;i<N;i++)         {             arr[i]=sc.nextInt();         }         Arrays.sort(arr);         System.out.print(arr[0]);     } }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int N;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        int arr[]=new int[N];
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.print(arr[0]);
+    }
+}
