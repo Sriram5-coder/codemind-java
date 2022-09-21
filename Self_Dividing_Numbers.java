27c94eecde2d59b45902ca241616f9e84d1d287b
K_SaiSriram <84832741+Sriram5-coder@users.noreply.github.com>
2022-09-21 10:07:29 +0530

    import java.util.Scanner; class Sample {     public static boolean checknum(int n)     {         int temp=n;         while(temp>0)         {             if(temp%10!=0)             {                 int j=temp%10;                 if(n%j!=0)                 {                     return false;                 }                 temp/=10;             }             else                 return false;         }         return true;     }     public static void main(String args[])     {         int N,M;         Scanner sc=new Scanner(System.in);         N=sc.nextInt();         M=sc.nextInt();         for(int i=N;i<=M;i++)         {             if(checknum(i))             {                 System.out.print(i+" ");             }         }
        }
    }

--- Self_Dividing_Numbers.java
+++ Self_Dividing_Numbers.java
@@ -1,24 +1,38 @@
 import java.util.Scanner;
-class Sample{
-    public static boolean divi(int n){
+class Sample
+{
+    public static boolean checknum(int n)
+    {
         int temp=n;
-        while(n>0){
-            int d = n%10;
-            if(d==0) return false;
-            if(temp%d!=0)
+        while(temp>0)
+        {
+            if(temp%10!=0)
+            {
+                int j=temp%10;
+                if(n%j!=0)
+                {
+                    return false;
+                }
+                temp/=10;
+            }
+            else
                 return false;
-            n/=10;
         }
         return true;
     }
-    public static void main(String args[]){
-        Scanner s = new Scanner(System.in);
-        int a = s.nextInt();
-        int b = s.nextInt();
-        for(int i=a ; i<=b ; i++){
-            if(divi(i)){
+    public static void main(String args[])
+    {
+        int N,M;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        M=sc.nextInt();
+        for(int i=N;i<=M;i++)
+        {
+            if(checknum(i))
+            {
                 System.out.print(i+" ");
             }
         }
+        
     }
 }
