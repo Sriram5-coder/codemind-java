a3a455a7a3c7417e283bf2974f28b3c191342db6
K_SaiSriram <84832741+Sriram5-coder@users.noreply.github.com>
2022-09-21 05:53:33 +0530

    import java.util.Scanner; class Sample {     public static void main(String args[])     {         int N;         Scanner sc=new Scanner(System.in);         N=sc.nextInt();         System.out.format("%.2f",(3.14)*N*N);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Sample
+{
+    public static void main(String args[])
+    {
+        int N;
+        Scanner sc=new Scanner(System.in);
+        N=sc.nextInt();
+        System.out.format("%.2f",(3.14)*N*N);
+        
+    }
+}
